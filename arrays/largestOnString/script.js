37dd59eaf697bfc64ddf33753224af80624d037d
1hisham <hisham1ahmmed@gmail.com>
2023-05-22 00:21:21 +0530

    feat: add more dsa

---
+++ arrays/largestOnString/script.js
@@ -0,0 +1,10 @@
+function longestWord(string){
+    let maxLength = 0;
+    let i = 0 ;
+    let maxWordStart = -1
+    let currentWordStart = -1
+    console.log(maxWordStart, maxLength)
+    console.log(string.slice(maxWordStart, maxWordStart+maxLength))
+}
+
+longestWord(" this longgggg ass")
