37dbc0fecdce719d7623e9b954758c03a53994e9
1hisham <hisham1ahmmed@gmail.com>
2023-05-28 22:27:25 +0530

    algorithm

--- arrays/maximumProduct/script.js
+++ arrays/maximumProduct/script.js
@@ -1,4 +1,4 @@
-let arrays =  [6, 1, 0, -4 , -5, -6                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           ]
+let arrays =  ["open", "source", "programming", "is", "fun"]
 
 
 function sortedArray(array) {
@@ -6,7 +6,7 @@ function sortedArray(array) {
   while (i < array.length) {
     let j = i + 1;
     while (j < array.length) {
-      if (array[i] < array[j]) {
+      if (array[i].length > array[j].length) {
         let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
@@ -17,18 +17,21 @@ function sortedArray(array) {
   }                            
   return array;
 }
+console.log(sortedArray(arrays));
 
-function productMax(productCount, array) {
-  console.log(array);
-  array = sortedArray(array);
+
+
+// function productMax(productCount, array) {
+//   console.log(array);
+//   array = sortedArray(array);
   
-  let total = array[0]
-  let i = 1;
-  while (i < productCount) {
-      total = total * array[i];
-    i++;
-  }
-  return total;
-}
+//   let total = array[0]
+//   let i = 1;
+//   while (i < productCount) {
+//       total = total * array[i];
+//     i++;
+//   }
+//   return total;
+// }
 
-console.log(productMax(3,arrays));
+// console.log(productMax(3,arrays));
