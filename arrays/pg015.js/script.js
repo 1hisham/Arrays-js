63a420e90830a0981ec262c792a2db1ba6652fcb
1hisham <hisham1ahmmed@gmail.com>
2023-06-03 11:20:17 +0530

    algorithms

--- arrays/pg015.js/script.js
+++ arrays/pg015.js/script.js
@@ -1,36 +1,36 @@
-const givenString = "A man, a plan, a canal, Panama!";
+let str = "Programming is fun and"
+// let i = 0;
+// let pos = 0;
+// let start = 0;
+// let word = ' '
+// while(i < str.length ){
+//     if(str[i] == " " || i == (str.length -1) ){
+//         start = pos
+//         pos = i
+//     while(pos >= start){
+//         word = word+str[pos]
+//         pos--
+//     }
+//     pos = i
+//     word +=" "
+//   }
+//     i++
 
-function vailedItems(str) {
-  str = str.toLowerCase();
-  let i = 0;
-  let count = 0;
-  console.log(str);
-  let arrayWithWords = [];
-  while (i < str.length) {
-    let ascci = str.charCodeAt(i);
-    if (ascci < 122 && ascci > 96) {
-      let words = String.fromCharCode(ascci);
-      arrayWithWords[count] = words;
-      count++;
+// }
+// console.log(word);
+
+let word = ""
+let i = 0;
+let reverseStr = ""
+while(i < str.length){
+    word =  str[i] + word
+    if(str[i] == " " ||i == str.length -1){
+        reverseStr+= " "
+        reverseStr += word 
+        word =""
     }
-    i++;
-  }
-  return arrayWithWords;
+    i++
 }
+console.log(reverseStr);
 
-function toCheckPalindrom(givenString) {
-  let filterArray = vailedItems(givenString);
-  let i = 0;
-  let j = filterArray.length - 1;
-  while (i < Math.floor(filterArray.length / 2)) {
-    if (filterArray[i] !== filterArray[j]) {
-      return false;
-    }
-    j--;
-    i++;
-  }
-  return true;
-}
-console.log(toCheckPalindrom(givenString));
 
-// toCheckPalindrom(givenString)
